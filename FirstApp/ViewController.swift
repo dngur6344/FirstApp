981cb0470cdd9a721790722c4cbd41e376d100d8
dngur6344 <dngur6344@gmail.com>
2019-03-25 15:26:04 +0900

    version 3

--- FirstApp/ViewController.swift
+++ FirstApp/ViewController.swift
@@ -11,14 +11,19 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
+    let messageArray = ["May the force be with you","Live long and prosper", "To infinity and beyond","Space is big. You just won't believe how vastly, hugely,mindbogglinly big it is"]
+    var index=0
+    
     override func viewDidLoad() {//프로그램을 구동시키면 맨 처음 이곳으로 온다.
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     @IBAction func doButtonTap(_ sender: UIButton) {
         print("Button touched")
-        self.messageLabel.text="Hello World"
+        let nextString = self.messageArray[index]
+        self.messageLabel.text=nextString
+        index += 1
     }
     
 }
